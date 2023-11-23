616176a68649679e6092721268b3542d04821899
Tutku Ince <tutku.ince@outlook.com>
2023-11-23 21:55:59 +0300

    shop me front end

---
+++ shop-me-web-parent/shop-me-frontend/src/main/java/com/shopme/site/ShopMeFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMeFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMeFrontendApplication.class, args);
+    }
+
+}
