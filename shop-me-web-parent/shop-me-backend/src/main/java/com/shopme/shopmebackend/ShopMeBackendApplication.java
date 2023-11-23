f40d4e94c214f3f25ad9a000aebdeaf842a8d7c2
Tutku Ince <tutku.ince@outlook.com>
2023-11-23 21:35:09 +0300

    shop-me-backend project initializer

--- shop-me-web-parent/shop-me-backend/src/main/java/com/shopme/shopmebackend/ShopMeBackendApplication.java
+++ shop-me-web-parent/shop-me-backend/src/main/java/com/shopme/shopmebackend/ShopMeBackendApplication.java
@@ -1,13 +1,13 @@
-package com.shopme.shopmewebparent;
+package com.shopme.shopmebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopMeWebParentApplication {
+public class ShopMeBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShopMeWebParentApplication.class, args);
+        SpringApplication.run(ShopMeBackendApplication.class, args);
     }
 
 }
