a1120f70f531a54c6d3d6c7dabedd3d79cf84ab0
Tutku Ince <tutku.ince@outlook.com>
2023-11-23 21:52:05 +0300

    configuration

--- shop-me-web-parent/shop-me-backend/src/main/java/com/shopme/admin/ShopMeBackendApplication.java
+++ shop-me-web-parent/shop-me-backend/src/main/java/com/shopme/admin/ShopMeBackendApplication.java
@@ -1,4 +1,4 @@
-package com.shopme.shopmebackend;
+package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
